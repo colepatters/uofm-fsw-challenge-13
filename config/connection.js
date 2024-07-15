687fc8c5ab4fa80d32f2b2038ad2cca7b2fa5c16
Cole Patterson <cole.patter@gmail.com>
2024-07-15 17:35:02 -0500

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.DB_URL ?? 'localhost',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
